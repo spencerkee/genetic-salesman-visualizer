f0981b9ba2ee8d5676d73445f3f5ed127daafb89
Spencer Kee <spencerkee7@gmail.com>
2016-04-21 17:24:24 -0400

    added diversification function

--- salesman.py
+++ salesman.py
@@ -50,7 +50,7 @@ def singlePointCrossover(parent1, parent2):
 
 #return 2 parents
 def rouletteSelection(fitness_list):
-	invertedList = [1/i for i in fitness_list]
+	invertedList = [1/i**2 for i in fitness_list]
 	max = sum(invertedList)
 	pick = random.uniform(0,max)
 	currentValue = 0
@@ -68,6 +68,15 @@ def mate(path_list, fitness_list):
 	return singlePointCrossover(parent1,parent2)
 
 def mutate(path_list, mutation_percent, swap_number):
+	if type(path_list[0]) == int:
+		if random.uniform(0,100) <= mutation_percent:
+			lst = list(path_list)
+			for l in range(swap_number):
+				indices = random.sample(range(0, len(lst)), 2)
+				j = indices[0]
+				k = indices[1]
+				lst[j], lst[k] = lst[k], lst[j]
+			return lst
 	return_list = []
 	for i in path_list:
 		if random.uniform(0,100) <= mutation_percent:
@@ -156,43 +165,91 @@ def computeBest(cities):
 			best_one = path
 	return best_one
 
-def main(num_cities, num_paths, num_generation, dimensions=[500,500], elite_num = 1):
+#if something occurs too often, mutate all except 1
+def diversify(path_list, threshold):
+	path_list.sort()
+	first = 0
+	second = 0
+	return_list = []
+	while second <= len(path_list):
+		if second == len(path_list):
+			# print 'block',path_list[first:second]
+			if len(path_list[first:second]) > threshold:
+				for i in range(first,second-1):
+					return_list.append(mutate(path_list[i],100,1))
+				return_list.append(path_list[first])
+			else:
+				return_list = return_list + path_list[first:second]
+		elif path_list[first] != path_list[second]:
+			# print 'block', path_list[first:second]
+			if len(path_list[first:second]) > threshold:
+				for i in range(first,second-1):
+					return_list.append(mutate(path_list[i],100,1))
+				return_list.append(path_list[first])
+			else:
+				return_list = return_list + path_list[first:second]
+			first = second
+		second += 1
+	return return_list
+
+def main(num_cities, num_paths, num_generation, dimensions=[500,500], elite_num=1,diversify_threshold=10):
 	cities = generateCities(num_cities,dimensions)
 	paths = generatePaths(num_paths, num_cities)
+	lowest_score = float('inf')
+	best_path = []
 	for generation in range(num_generation):
 		new_generation = []
 		fitnesses = []
 		for path in paths:
 			fitnesses.append(fitness(path,cities))
 
-		# count = collections.Counter(paths)
-		# print len(count.most_common())
+		if min(fitnesses) < lowest_score:
+			best_path = paths[fitnesses.index(min(fitnesses))]
+			lowest_score = min(fitnesses)
 
-		lowest = paths[fitnesses.index(min(fitnesses))]
-		# print lowest
-		print sum(fitnesses)/num_paths, numUnique(paths)
-		# print paths
-		
+		# print sum(fitnesses)/num_paths, numUnique(paths)
 
 		while len(new_generation) < num_paths-elite_num:
 			new_generation.append(mate(paths,fitnesses))
+		
 		fit_copy = list(fitnesses)
 		fit_copy.sort()
 		for i in fit_copy[:elite_num]:
 			new_generation.append(paths[fitnesses.index(i)])
-		new_generation = mutate(new_generation, 1, 1)
+		
+		new_generation = mutate(new_generation, 2, 1)
 		paths = new_generation
+		paths = diversify(paths,diversify_threshold)
 
-	makeCluster(dimensions,paths,cities)
-	# BEST = computeBest(cities)
-	# print BEST, fitness(BEST,cities)
+	# makeCluster(dimensions,paths,cities)
+	BEST = computeBest(cities)
+	if fitness(BEST,cities) != lowest_score:
+		return 0
+	else:
+		return 1
+	# print 'best', BEST, fitness(BEST,cities)
+	# print 'mybest', best_path, lowest_score
 	# makeImage(dimensions, lowest, cities)
 
 
-main(7,7,1)
+
+
+def tester(num_trials, generations):
+	result_list = []
+	for trial in range(num_trials):
+		print 'trialnum', trial
+		result_list.append(main(num_cities=8,num_paths=50,num_generation=generations))
+	return sum(result_list)
+
+
+# 100 trials 30 generations 41%
+# 100 trials 40 generations 49%
+# 100 trials 50 generations 54%
+print tester(100,50)
 
 
 
+# main(num_cities=8,num_paths=50,num_generation=30)
 # cities = generateCities(10,[100,100])#took 54 seconds
 # computeBest(cities)
 
